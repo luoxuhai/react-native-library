2d5944c62a994ccadbe480c05aaaff217d4927f6
Darkce <darkce97@gmail.com>
2022-12-12 23:09:28 +0800

    feat: add alert-ios

---
+++ packages/alert-ios/ios/AlertIos.swift
@@ -0,0 +1,8 @@
+@objc(AlertIos)
+class AlertIos: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
